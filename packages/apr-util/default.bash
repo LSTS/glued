1b1e6b3fd7e9e8a5f0ec397656ad8ff30689c76e
Ricardo Martins <rasm@lsts.pt>
2013-07-13 18:15:44 +0100

    apr-util: changed download URL.

--- packages/apr-util/default.bash
+++ packages/apr-util/default.bash
@@ -54,12 +54,12 @@ host_install()
 
 target_install()
 {
-  # Libs:
-  for f in "$cfg_dir_toolchain_sysroot/usr/lib/libaprutil"*".so"*; do
-    if [ -L "$f" ]; then
-      $cmd_cp "$f" "$cfg_dir_rootfs/usr/lib"
-    else
-      $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
-   fi
-  done
+    # Libs:
+    for f in "$cfg_dir_toolchain_sysroot/usr/lib/libaprutil"*".so"*; do
+        if [ -L "$f" ]; then
+            $cmd_cp "$f" "$cfg_dir_rootfs/usr/lib"
+        else
+            $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
+        fi
+    done
 }
--- packages/apr-util/ros.bash
+++ packages/apr-util/ros.bash
@@ -5,7 +5,7 @@ version=\
 
 url=\
 (
-    "ftp://cis.uniroma2.it/unix/packages/APACHE/apr/apr-util-${version}.tar.bz2"
+    "http://www.eu.apache.org/dist/apr/apr-util-$version.tar.bz2"
 )
 
 md5=\
@@ -20,25 +20,25 @@ requires=\
 
 configure()
 {
-  cd apr-util-${version}
-  ./configure \
-            --prefix="${cfg_dir_toolchain_sysroot}/usr" \
-            --target="${cfg_target_canonical}" \
-            --host="${cfg_target_canonical}" \
-            --build="${cfg_host_canonical}" \
-            --enable-shared \
-            --disable-static \
-            --without-pgsql \
-            --without-sqlite2 \
-            --without-sqlite3 \
-            --with-apr="${cfg_dir_toolchain_sysroot}/usr" \
-            --with-expat=builtin \
-            ac_cv_file__dev_zero=yes \
-            ac_cv_func_setpgrp_void=yes \
-            apr_cv_tcp_nodelay_with_cork=no \
-            apr_cv_process_shared_works=no \
-            apr_cv_mutex_robust_shared=no \
-            ac_cv_sizeof_struct_iovec=8
+    cd apr-util-${version}
+    ./configure \
+        --prefix="${cfg_dir_toolchain_sysroot}/usr" \
+        --target="${cfg_target_canonical}" \
+        --host="${cfg_target_canonical}" \
+        --build="${cfg_host_canonical}" \
+        --enable-shared \
+        --disable-static \
+        --without-pgsql \
+        --without-sqlite2 \
+        --without-sqlite3 \
+        --with-apr="${cfg_dir_toolchain_sysroot}/usr" \
+        --with-expat=builtin \
+        ac_cv_file__dev_zero=yes \
+        ac_cv_func_setpgrp_void=yes \
+        apr_cv_tcp_nodelay_with_cork=no \
+        apr_cv_process_shared_works=no \
+        apr_cv_mutex_robust_shared=no \
+        ac_cv_sizeof_struct_iovec=8
 }
 
 build()
@@ -53,12 +53,12 @@ host_install()
 
 target_install()
 {
-  # Libs: @todo
-  for f in "$cfg_dir_toolchain_sysroot/usr/lib/"libaprutil-*; do
-    if [ -L "$f" ]; then
-      $cmd_cp "$f" "$cfg_dir_rootfs/usr/lib"
-    else
-      $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
-   fi
-  done
+    # Libs: @todo
+    for f in "$cfg_dir_toolchain_sysroot/usr/lib/"libaprutil-*; do
+        if [ -L "$f" ]; then
+            $cmd_cp "$f" "$cfg_dir_rootfs/usr/lib"
+        else
+            $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
+        fi
+    done
 }
