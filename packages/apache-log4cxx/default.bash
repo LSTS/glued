7b6b5e042bd9febb423e9d031e92cd5e5ec95f02
Ricardo Martins <rasm@lsts.pt>
2013-07-13 18:16:05 +0100

    apache-log4cxx: changed download URL.

--- packages/apache-log4cxx/default.bash
+++ packages/apache-log4cxx/default.bash
@@ -1,22 +1,22 @@
 version=\
 (
-  "0.10.0"
+    '0.10.0'
 )
 
 url=\
 (
-  "http://apache.fis.uniroma2.it/logging/log4cxx/0.10.0/apache-log4cxx-0.10.0.tar.gz"
+    "http://www.eu.apache.org/dist/logging/log4cxx/0.10.0/apache-log4cxx-0.10.0.tar.gz"
 )
 
 md5=\
 (
-  "b30ffb8da3665178e68940ff7a61084c"
+    'b30ffb8da3665178e68940ff7a61084c'
 )
 
 requires=\
 (
-  "apr/default"
-  "apr-util/default"
+    'apr/default'
+    'apr-util/default'
 )
 
 post_unpack()
@@ -29,43 +29,42 @@ post_unpack()
 
 configure()
 {
-  ./configure \
-            --prefix="${cfg_dir_toolchain_sysroot}/usr"        \
-            --target=${cfg_target_canonical} \
-            --host=${cfg_target_canonical}   \
-            --build=${cfg_host_canonical}    \
-            --with-apr="${cfg_dir_toolchain_sysroot}/usr"      \
-            --with-apr-util="${cfg_dir_toolchain_sysroot}/usr" \
-            --enable-shared                  \
-            --disable-static                 \
-            ac_cv_file__dev_zero=yes         \
-            ac_cv_func_setpgrp_void=yes      \
-            apr_cv_tcp_nodelay_with_cork=no  \
-            apr_cv_process_shared_works=no   \
-            apr_cv_mutex_robust_shared=no    \
-            ac_cv_sizeof_struct_iovec=8
+    ./configure \
+        --prefix="${cfg_dir_toolchain_sysroot}/usr"        \
+        --target=${cfg_target_canonical} \
+        --host=${cfg_target_canonical}   \
+        --build=${cfg_host_canonical}    \
+        --with-apr="${cfg_dir_toolchain_sysroot}/usr"      \
+        --with-apr-util="${cfg_dir_toolchain_sysroot}/usr" \
+        --enable-shared                  \
+        --disable-static                 \
+        ac_cv_file__dev_zero=yes         \
+        ac_cv_func_setpgrp_void=yes      \
+        apr_cv_tcp_nodelay_with_cork=no  \
+        apr_cv_process_shared_works=no   \
+        apr_cv_mutex_robust_shared=no    \
+        ac_cv_sizeof_struct_iovec=8
 }
 
 build()
 {
-  $cmd_make 
+    $cmd_make
 }
 
 host_install()
 {
-  cd ../log4cpp
-  $cmd_make install
+    cd ../log4cpp
+    $cmd_make install
 }
 
 target_install()
 {
-  # Libs: 
-  for f in "$cfg_dir_toolchain_sysroot/usr/lib/"liblog4cxx*.so*; do
-    if [ -L "$f" ]; then
-      $cmd_cp "$f" "$cfg_dir_rootfs/usr/lib"
-    else
-      $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
-    fi
-  done
+    # Libs:
+    for f in "$cfg_dir_toolchain_sysroot/usr/lib/"liblog4cxx*.so*; do
+        if [ -L "$f" ]; then
+            $cmd_cp "$f" "$cfg_dir_rootfs/usr/lib"
+        else
+            $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
+        fi
+    done
 }
-
