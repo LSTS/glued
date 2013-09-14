eb7b97af9923afc8826a61c96eba055ff9ad3a8b
Jose Pinto <zepinto@gmail.com>
2013-09-14 23:49:40 +0100

    Added node.js package.

---
+++ packages/nodejs/default.bash
@@ -0,0 +1,38 @@
+version=\
+(
+    '0.10.18'
+)
+
+url=\
+(
+    "http://nodejs.org/dist/v0.10.18/node-v$version.tar.gz"
+)
+
+md5=\
+(
+    '8b905b6013b82fb7076b02a6c881f9a6'
+)
+
+maintainer=\
+(
+    'Jose Pinto <zepinto@fe.up.pt>'
+)
+
+configure()
+{
+    "../node-v$version/configure" --without-snapshot --prefix="${cfg_dir_rootfs}"
+}
+
+build()
+{
+    cd "../node-v$version"
+    $cmd_make
+}
+
+target_install()
+{
+    cd "../node-v$version"
+    $cmd_make install
+}
+
+
