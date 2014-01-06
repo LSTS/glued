5bda410e3e876f3707d97faf11f79734d914bd8d
Ricardo Martins <rasm@lsts.pt>
2014-01-06 18:45:50 +0000

    Starting v2.0.x cycle.

--- mkconfig.bash
+++ mkconfig.bash
@@ -21,7 +21,7 @@
 ###########################################################################
 
 # Config: GLUED version.
-cfg_glued_version='1.9.0'
+cfg_glued_version='2.0.x'
 # Config: GLUED vendor.
 cfg_glued_vendor='lsts'
 
