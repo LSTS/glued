a0efadc2d5efbaf6120d514738844bc5e41748cc
Ricardo Martins <rasm@lsts.pt>
2014-01-06 17:07:23 +0000

    libpng: updated to 1.6.8.

--- packages/libpng/default.bash
+++ packages/libpng/default.bash
@@ -1,46 +1,45 @@
 version=\
 (
-  "1.6.2"
+    '1.6.8'
 )
 
 url=\
 (
-  "http://downloads.sourceforge.net/project/libpng/libpng16/$version/libpng-$version.tar.xz"
+    "http://downloads.sourceforge.net/project/libpng/libpng16/$version/libpng-$version.tar.xz"
 )
 
 md5=\
 (
-  "9d838f6fca9948a9f360a0cc1b516d5f"
+    '51ce71a1642cdde1f4485a7ff82193c0'
 )
 
 configure()
 {
-  ./configure \
-    --prefix="${cfg_dir_toolchain_sysroot}/usr" \
-    --target="$cfg_target_canonical"            \
-    --host="$cfg_target_canonical"              \
-    --build="$cfg_host_canonical"
+    ./configure \
+        --prefix="${cfg_dir_toolchain_sysroot}/usr" \
+        --target="$cfg_target_canonical"            \
+        --host="$cfg_target_canonical"              \
+        --build="$cfg_host_canonical"
 }
 
 build()
 {
-  $cmd_make
+    $cmd_make
 }
 
 host_install()
 {
-  $cmd_make install
+    $cmd_make install
 }
 
 target_install()
 {
-  # Libs: 
-  for f in "$cfg_dir_toolchain_sysroot/usr/lib/"libpng*.so*; do
-    if [ -L "$f" ]; then
-      $cmd_cp "$f" "$cfg_dir_rootfs/usr/lib"
-    else
-      $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
-    fi
-  done
+    # Libs:
+    for f in "$cfg_dir_toolchain_sysroot/usr/lib/"libpng*.so*; do
+        if [ -L "$f" ]; then
+            $cmd_cp "$f" "$cfg_dir_rootfs/usr/lib"
+        else
+            $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
+        fi
+    done
 }
-
