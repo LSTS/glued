c2c0689b700f87f10299f06122d28956d5498f0e
Jose Braga <eejbraga@gmail.com>
2014-01-28 10:49:04 +0000

    Marking version 1.9.4.

--- mkconfig.bash
+++ mkconfig.bash
@@ -21,7 +21,7 @@
 ###########################################################################
 
 # Config: GLUED version.
-cfg_glued_version='1.9.3'
+cfg_glued_version='1.9.4'
 # Config: GLUED vendor.
 cfg_glued_vendor='lsts'
 
