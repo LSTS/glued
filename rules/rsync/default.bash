dd980c2220c18011ae3826d9c75bece15e2a252c
Ricardo Martins <rasm@lsts.pt>
2014-04-28 22:22:22 +0100

    rsync: added daemon script and config file.

--- rules/rsync/default.bash
+++ rules/rsync/default.bash
@@ -46,5 +46,6 @@ build()
 
 target_install()
 {
-    $cmd_target_strip rsync -o "$cfg_dir_rootfs/usr/bin/rsync"
+    $cmd_target_strip rsync -o "$cfg_dir_rootfs/usr/bin/rsync" &&
+    tar -C "$pkg_dir/fs" --exclude .svn -c -f - . | tar -C "$cfg_dir_rootfs" -x -v -f -
 }
