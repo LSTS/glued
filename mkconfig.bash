c00c27649bace47acf2085cac19a0765f7e1056b
Ricardo Martins <rasm@lsts.pt>
2014-04-29 03:42:11 +0100

    Version 1.9.8.

--- mkconfig.bash
+++ mkconfig.bash
@@ -21,7 +21,7 @@
 ###########################################################################
 
 # Config: GLUED version.
-cfg_glued_version='1.9.7'
+cfg_glued_version='1.9.8'
 # Config: GLUED vendor.
 cfg_glued_vendor='lsts'
 
