00b616b7c7fe6787a338d654422a7fc5bcd4f2a0
Jose Braga <eejbraga@gmail.com>
2014-05-05 14:25:33 +0100

    Development version 1.9.x.

--- mkconfig.bash
+++ mkconfig.bash
@@ -21,7 +21,7 @@
 ###########################################################################
 
 # Config: GLUED version.
-cfg_glued_version='1.9.9'
+cfg_glued_version='1.9.x'
 # Config: GLUED vendor.
 cfg_glued_vendor='lsts'
 
