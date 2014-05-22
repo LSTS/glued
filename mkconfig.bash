2e2826068252723bc2ac15247e8f6cdf67d4dfcf
Ricardo Martins <rasm@lsts.pt>
2014-05-22 15:21:14 +0100

    Starting v1.12.x development cycle.

--- mkconfig.bash
+++ mkconfig.bash
@@ -23,7 +23,7 @@
 ###########################################################################
 
 # Config: GLUED version.
-cfg_glued_version='1.11.x'
+cfg_glued_version='1.12.x'
 # Config: GLUED vendor.
 cfg_glued_vendor='lsts'
 
