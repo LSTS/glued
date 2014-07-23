9e3c75a0ab301058d108dcf65497b8be0f2c237a
Ricardo Martins <rasm@lsts.pt>
2014-07-23 18:51:58 +0100

    tcpdump: updated to version 4.6.1.

--- rules/tcpdump/default.bash
+++ rules/tcpdump/default.bash
@@ -1,16 +1,16 @@
 version=\
 (
-    4.3.0
+    '4.6.1'
 )
 
 url=\
 (
-    http://www.tcpdump.org/release/tcpdump-$version.tar.gz
+    "http://www.tcpdump.org/release/tcpdump-$version.tar.gz"
 )
 
 md5=\
 (
-    a3fe4d30ac85ff5467c889ff46b7e1e8
+    'dab267ec30216a069747d10314079ec7'
 )
 
 requires=\
@@ -21,7 +21,7 @@ requires=\
 configure()
 {
     echo 'ac_cv_linux_vers=${ac_cv_linux_vers=2}' > config.cache
-    ../tcpdump-$version/configure -C \
+    "../tcpdump-$version/configure" -C \
         --prefix="$cfg_dir_toolchain_sysroot/usr" \
         --disable-static \
         --enable-shared \
@@ -29,7 +29,8 @@ configure()
         --host=$cfg_target_canonical \
         --build=$cfg_host_canonical \
         --includedir="$cfg_dir_toolchain_sysroot/usr/include" \
-        --libdir="$cfg_dir_toolchain_sysroot/usr/lib"
+        --libdir="$cfg_dir_toolchain_sysroot/usr/lib" \
+        --disable-ipv6
 }
 
 build()
