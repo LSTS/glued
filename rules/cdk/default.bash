346e4b60a78686d0ed3caef1b106a1af9d02dbc2
Ricardo Martins <rasm@lsts.pt>
2014-08-07 15:24:25 +0100

    cdk: updated download URL.

--- rules/cdk/default.bash
+++ rules/cdk/default.bash
@@ -1,16 +1,16 @@
 version=\
 (
-  "5.0-20120323"
+    '5.0-20140118'
 )
 
 url=\
 (
-  "ftp://invisible-island.net/cdk/cdk.tar.gz"
+    "ftp://invisible-island.net/cdk/cdk-$version.tgz"
 )
 
 md5=\
 (
-  "929969d06ce034e862976bb7d1a36b25"
+    'd2687e5558b6d6be5fd39328e5c86033'
 )
 
 requires=\
@@ -20,34 +20,34 @@ requires=\
 
 configure()
 {
-#        --target="${cfg_target_canonical}" \
-  ./configure \
-    --prefix="$cfg_dir_toolchain_sysroot/usr" \
-    --host="${cfg_target_canonical}" \
-    --build="${cfg_host_canonical}" \
+    #        --target="${cfg_target_canonical}" \
+    ./configure \
+        --prefix="$cfg_dir_toolchain_sysroot/usr" \
+        --host="${cfg_target_canonical}" \
+        --build="${cfg_host_canonical}" \
         --target="${cfg_target_canonical}" \
-    --with-ncursesw \
-    --with-shared   \
+        --with-ncursesw \
+        --with-shared   \
 
 }
 
 build()
 {
-  $cmd_make
+    $cmd_make
 }
 
 host_install()
 {
-  $cmd_make install
+    $cmd_make install
 }
 
 target_install()
 {
-  for f in "$cfg_dir_toolchain_sysroot/usr/lib/libcdk"* ; do
-    if [ -L "$f" ]; then
-      cp -av "$f" "$cfg_dir_rootfs/usr/lib"
-    else
-      $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
-    fi
-  done
+    for f in "$cfg_dir_toolchain_sysroot/usr/lib/libcdk"* ; do
+        if [ -L "$f" ]; then
+            cp -av "$f" "$cfg_dir_rootfs/usr/lib"
+        else
+            $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
+        fi
+    done
 }
