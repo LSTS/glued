f1d49eba03d0ce8c57eb23e0b08d0a636011ed9f
Ricardo Martins <rasm@lsts.pt>
2014-08-07 15:24:08 +0100

    gnuplot: upgraded to version 4.6.5.

--- rules/gnuplot/default.bash
+++ rules/gnuplot/default.bash
@@ -1,41 +1,41 @@
 version=\
 (
-  "4.6.1"
+    '4.6.5'
 )
 
 url=\
 (
-  "ftp://ftp.dante.de/pub/tex/graphics/gnuplot/${version}/gnuplot-${version}.tar.gz"
+    "http://downloads.sourceforge.net/gnuplot/gnuplot/$version/gnuplot-$version.tar.gz"
 )
 
 md5=\
 (
-  "4c9a06461f402482c30cf94e267eb877"
+    '9a476b21f74bd99c876f1509d731a0f9'
 )
 
 configure()
 {
-  ./configure \
-   --prefix="${cfg_dir_toolchain_sysroot}/usr" \
-   --target="$cfg_target_canonical" \
-   --host="$cfg_target_canonical" \
-   --build="$cfg_host_canonical" \
-   --disable-history-file \
-   --without-x \
-   --disable-raise-console \
-   --disable-mouse \
-   --without-tutorial \
-   --without-demo \
-   --disable-demo \
-   --without-row-help \
-   --disable-history-file \
-   --without-lisp-files \
-   --disable-wxwidgets \
-   --without-lua \
-   --without-latex \
-   --without-cairo
+    ./configure \
+        --prefix="${cfg_dir_toolchain_sysroot}/usr" \
+        --target="$cfg_target_canonical" \
+        --host="$cfg_target_canonical" \
+        --build="$cfg_host_canonical" \
+        --disable-history-file \
+        --without-x \
+        --disable-raise-console \
+        --disable-mouse \
+        --without-tutorial \
+        --without-demo \
+        --disable-demo \
+        --without-row-help \
+        --disable-history-file \
+        --without-lisp-files \
+        --disable-wxwidgets \
+        --without-lua \
+        --without-latex \
+        --without-cairo
 
-  sed -i "s#demo ##g" Makefile
+    sed -i "s#demo ##g" Makefile
 }
 
 build()
@@ -50,8 +50,6 @@ host_install()
 
 target_install()
 {
-  $cmd_target_strip -v "$cfg_dir_toolchain_sysroot/usr/bin/gnuplot" -o 	"$cfg_dir_rootfs/usr/bin/gnuplot"
-  $cmd_cp "$cfg_dir_toolchain_sysroot/usr/share/gnuplot" 		"$cfg_dir_rootfs/usr/share"
+    $cmd_target_strip -v "$cfg_dir_toolchain_sysroot/usr/bin/gnuplot" -o 	"$cfg_dir_rootfs/usr/bin/gnuplot"
+    $cmd_cp "$cfg_dir_toolchain_sysroot/usr/share/gnuplot" 		"$cfg_dir_rootfs/usr/share"
 }
-
-
