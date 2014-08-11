5c2bc141dffdec4c2d4c060f10d097c526b3fc41
Ricardo Martins <rasm@lsts.pt>
2014-08-11 16:43:40 +0100

    u-boot: upgraded to version 2014.07.

--- rules/u-boot/default.bash
+++ rules/u-boot/default.bash
@@ -1,8 +1,6 @@
-source "$pkg_common"
-
 version=\
 (
-    '2014.01'
+    '2014.07'
 )
 
 url=\
@@ -12,5 +10,44 @@ url=\
 
 md5=\
 (
-    'e531307578f6d32a7ccb1d04f1e08cbc'
+    '36d4bad687edcafa396fee607e505d4e'
 )
+
+post_unpack()
+{
+    patches=$(ls "$pkg_dir/patches-$version/"*.patch\
+	"$cfg_dir_system/patches/u-boot/patches-${version}/"*.patch 2>/dev/null)
+    if [ -n "$patches" ]; then
+        cat $patches | patch -p1
+    fi
+}
+
+configure()
+{
+    $cmd_make \
+        "${cfg_target_uboot_config}_config"
+}
+
+build()
+{
+    $cmd_make \
+        CROSS_COMPILE="$cfg_target_canonical"-
+}
+
+host_install()
+{
+    $cmd_cp \
+        tools/mkimage \
+        "$cfg_dir_toolchain/bin"
+}
+
+target_install()
+{
+    if [ -f MLO ]; then
+        $cmd_cp MLO "$cfg_dir_rootfs/boot"
+    fi
+
+    if [ -f u-boot.img ]; then
+        $cmd_cp u-boot.img "$cfg_dir_rootfs/boot"
+    fi
+}
