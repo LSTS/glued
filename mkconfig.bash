d1aede925a683ecc6ae9eb88b3c3a2d9181675a0
Ricardo Martins <rasm@lsts.pt>
2014-08-11 17:09:00 +0100

    Preparing v1.9.12.

--- mkconfig.bash
+++ mkconfig.bash
@@ -21,7 +21,7 @@
 ###########################################################################
 
 # Config: GLUED version.
-cfg_glued_version='1.9.11'
+cfg_glued_version='1.9.12'
 # Config: GLUED vendor.
 cfg_glued_vendor='lsts'
 
