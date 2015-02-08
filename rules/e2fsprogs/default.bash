dc9a0be12f6c1b5a0832aa32425ebcab19c719ff
Ricardo Martins <rasm@lsts.pt>
2015-02-08 20:53:58 +0000

    e2fsprogs: added mkfs.* and fsck.* symlinks.

--- rules/e2fsprogs/default.bash
+++ rules/e2fsprogs/default.bash
@@ -20,7 +20,13 @@ build()
 
 target_install()
 {
-    $cmd_target_strip --strip-unneeded ../e2fsprogs-$version/e2fsck/e2fsck -o $cfg_dir_rootfs/sbin/e2fsck &&
-    $cmd_target_strip --strip-unneeded ../e2fsprogs-$version/misc/mke2fs -o $cfg_dir_rootfs/sbin/mke2fs &&
-    $cmd_target_strip --strip-unneeded ../e2fsprogs-$version/misc/tune2fs -o $cfg_dir_rootfs/sbin/tune2fs
+    $cmd_target_strip --strip-unneeded ../e2fsprogs-$version/e2fsck/e2fsck -o "$cfg_dir_rootfs/sbin/e2fsck" &&
+    $cmd_target_strip --strip-unneeded ../e2fsprogs-$version/misc/mke2fs -o "$cfg_dir_rootfs/sbin/mke2fs" &&
+    $cmd_target_strip --strip-unneeded ../e2fsprogs-$version/misc/tune2fs -o "$cfg_dir_rootfs/sbin/tune2fs" &&
+    ln -fs e2fsck "$cfg_dir_rootfs/sbin/fsck.ext2" &&
+    ln -fs e2fsck "$cfg_dir_rootfs/sbin/fsck.ext3" &&
+    ln -fs e2fsck "$cfg_dir_rootfs/sbin/fsck.ext4" &&
+    ln -fs mke2fs "$cfg_dir_rootfs/sbin/mkfs.ext2" &&
+    ln -fs mke2fs "$cfg_dir_rootfs/sbin/mkfs.ext3" &&
+    ln -fs mke2fs "$cfg_dir_rootfs/sbin/mkfs.ext4"
 }
