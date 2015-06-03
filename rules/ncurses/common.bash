1a1b50467626a9228c32df8ce5d7658905a3f86d
Ricardo Martins <rasm@oceanscan-mst.com>
2015-06-03 17:12:41 +0100

    ncurses: fixed build with gcc 5.

--- rules/ncurses/common.bash
+++ rules/ncurses/common.bash
@@ -17,3 +17,13 @@ maintainer=\
 (
     "Ricardo Martins <rasm@fe.up.pt>"
 )
+
+post_unpack()
+{
+    patches=$(ls "$pkg_dir"/patches/*.patch)
+
+    cd "../$pkg-$version"
+    if [ -n "$patches" ]; then
+        cat $patches | patch -p1
+    fi
+}
