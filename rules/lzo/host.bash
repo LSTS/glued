21afcd85668f45d134406bbd0b60ad2df847e344
Ricardo Martins <rasm@oceanscan-mst.com>
2015-09-04 17:46:37 +0100

    lzo: added default rule.

--- rules/lzo/host.bash
+++ rules/lzo/host.bash
@@ -1,22 +1,4 @@
-version=\
-(
-    '2.03'
-)
-
-url=\
-(
-    "http://www.oberhumer.com/opensource/lzo/download/lzo-$version.tar.gz"
-)
-
-md5=\
-(
-    '0c3d078c2e8ea5a88971089a2f02a726'
-)
-
-maintainer=\
-(
-    'Ricardo Martins <rasm@fe.up.pt>'
-)
+. "$pkg_common"
 
 configure()
 {
