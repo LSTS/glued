8f7fc2b4520f833f4457d4be2200fb42efd41352
Ricardo Martins <rasm@oceanscan-mst.com>
2015-09-28 16:11:36 +0100

    kmod: upgraded to v21, added support for target builds.

---
+++ rules/kmod/common.bash
@@ -0,0 +1,19 @@
+version=\
+(
+    '21'
+)
+
+url=\
+(
+    "http://ftp.kernel.org/pub/linux/utils/kernel/kmod/kmod-$version.tar.xz"
+)
+
+md5=\
+(
+    'ee246fab2e1cba9fbdcad6a86ec31531'
+)
+
+maintainer=\
+(
+    'Ricardo Martins <rasm@oceanscan-mst.com>'
+)
