4b4ebc0a5d664b66d704f4edfe8c0dee74aeeb02
Ricardo Martins <rasm@lsts.pt>
2015-10-24 13:12:09 +0100

    GLUED v1.12.2.

--- mkconfig.bash
+++ mkconfig.bash
@@ -23,7 +23,7 @@
 ###########################################################################
 
 # Config: GLUED version.
-cfg_glued_version='1.12.1'
+cfg_glued_version='1.12.2'
 # Config: GLUED vendor.
 cfg_glued_vendor='lsts'
 
