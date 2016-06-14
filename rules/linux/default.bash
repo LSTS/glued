6b7b82786e02e1437243bb09c2c74a97bd4f34a8
Ricardo Martins <rasm@oceanscan-mst.com>
2016-06-14 19:30:56 +0100

    linux: forced depmod path.

--- rules/linux/default.bash
+++ rules/linux/default.bash
@@ -128,12 +128,14 @@ build()
     $cmd_make \
         CROSS_COMPILE=$cfg_target_canonical- \
         ARCH=$cfg_target_linux \
+        DEPMOD="$cfg_dir_toolchain/sbin/depmod" \
         modules || return 1
 
     if [ "$(basename $cfg_target_linux_kernel)" = 'uImage' ]; then
         $cmd_make \
             CROSS_COMPILE=$cfg_target_canonical- \
             ARCH=$cfg_target_linux \
+            DEPMOD="$cfg_dir_toolchain/sbin/depmod" \
             uImage || return 1
     fi
 
@@ -142,6 +144,7 @@ build()
         $cmd_make \
             CROSS_COMPILE=$cfg_target_canonical- \
             ARCH=$cfg_target_linux \
+            DEPMOD="$cfg_dir_toolchain/sbin/depmod" \
             zImage || return 1
     fi
 
@@ -150,6 +153,7 @@ build()
         $cmd_make \
             CROSS_COMPILE=$cfg_target_canonical- \
             ARCH=$cfg_target_linux \
+            DEPMOD="$cfg_dir_toolchain/sbin/depmod" \
             dtbs || return 1
     fi
 
@@ -193,11 +197,15 @@ target_install()
         CROSS_COMPILE="$cfg_target_canonical-" \
         ARCH="$cfg_target_linux" \
         INSTALL_MOD_PATH="$cfg_dir_rootfs/usr" \
+        KBUILD_VERBOSE=1 \
+        DEPMOD="$cfg_dir_toolchain/sbin/depmod" \
         modules_install
 
     $cmd_make \
         CROSS_COMPILE="$cfg_target_canonical-" \
         ARCH="$cfg_target_linux" \
         INSTALL_MOD_PATH="$cfg_dir_rootfs/usr" \
+        KBUILD_VERBOSE=1 \
+        DEPMOD="$cfg_dir_toolchain/sbin/depmod" \
         firmware_install
 }
