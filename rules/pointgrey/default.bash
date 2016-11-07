9d43ffd5e8133326cff3732a8b357ccad6b47c25
Pedro Gonçalves <pedro@lsts.pt>
2016-11-07 17:40:57 +0000

    pointgrey: update rule.

--- rules/pointgrey/default.bash
+++ rules/pointgrey/default.bash
@@ -13,6 +13,12 @@ md5=\
   "ffee545c0353f1a31008ad3166b1f057"
 )
 
+post_unpack()
+{
+    $cmd_cp -r "../flycapture.2.10.3.237_armhf/"* "../$cfg_packages-$version/"
+    rm -r ../flycapture.2.10.3.237_armhf/
+}
+
 requires=\
 (
     'libdc1394/default'
@@ -26,12 +32,12 @@ maintainer=\
 
 host_install()
 {
-  $cmd_cp -r "../flycapture.2.10.3.237_armhf/lib/"*    "$cfg_dir_toolchain_sysroot/usr/lib/"
-  $cmd_cp -r "../flycapture.2.10.3.237_armhf/include/"* "$cfg_dir_toolchain_sysroot/usr/include/"
+  $cmd_cp -r "lib/"*    "$cfg_dir_toolchain_sysroot/usr/lib/"
+  $cmd_cp -r "include/"* "$cfg_dir_toolchain_sysroot/usr/include/"
 }
 
 target_install()
 {
-    $cmd_cp -r "../flycapture.2.10.3.237_armhf/lib/"* "$cfg_dir_rootfs/usr/lib/"
+    $cmd_cp -r "lib/"* "$cfg_dir_rootfs/usr/lib/"
 }
 
