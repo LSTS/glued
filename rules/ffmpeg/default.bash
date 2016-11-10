b9e2e9c2b8ec9973d4a5ff2a1cbd6dfe2adb651c
Pedro Gonçalves <pedro@lsts.pt>
2016-11-10 19:17:21 +0000

    ffmpeg: add libs (*.so) to sysroot of toolchain.

--- rules/ffmpeg/default.bash
+++ rules/ffmpeg/default.bash
@@ -15,9 +15,10 @@ md5=\
 
 configure()
 {
+    $cmd_mkdir build
     ./configure \
         --sysroot="$cfg_dir_toolchain_sysroot" \
-        --prefix="$cfg_dir_rootfs/usr" \
+        --prefix="$cfg_dir_builds/ffmpeg/ffmpeg-$version/build/" \
         --incdir="$cfg_dir_toolchain_sysroot/usr/include" \
         --enable-cross-compile \
         --cross-prefix="$cfg_target_canonical-" \
@@ -39,10 +40,13 @@ host_install()
     $cmd_make \
 	LIBDIR="$cfg_dir_toolchain_sysroot/usr/lib" \
         install-headers
+    $cmd_make install
+    $cmd_cp -r "build/"*        "$cfg_dir_toolchain_sysroot/usr/"
 }
 
 target_install()
 {
     $cmd_make \
         install-libs
+    $cmd_cp -r "build/"*        "$cfg_dir_rootfs/usr/"
 }
