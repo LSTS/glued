24f7c187a99ef34a8a8763e393448e1f30474acb
Pedro Gonçalves <pedro@lsts.pt>
2017-01-11 15:24:17 +0000

    rpi-boot-firmware: add new url/package of boot rpi firmware.

--- rules/rpi-boot-firmware/default.bash
+++ rules/rpi-boot-firmware/default.bash
@@ -1,21 +1,22 @@
 version=\
 (
-    '2016-05-17'
+    '2016-10-20'
 )
 
 url=\
 (
-    "https://github.com/rasmartins/rpi-boot-firmware/archive/rpi-boot-firmware-$version.tar.gz"
+    "http://lsts.pt/glued/rpi-boot-firmware-$version.zip"
 )
 
 maintainer=\
 (
     'Ricardo Martins <rasm@fe.up.pt>'
+    'Pedro Gonçalves <pedro@lsts.pt>'
 )
 
 md5=\
 (
-    '0f2591003dc1cf6b5550a28e56d06f40'
+    'a5310b72332031e617518f321bf3c61e'
 )
 
 target_install()
@@ -23,8 +24,8 @@ target_install()
     $cmd_mkdir \
         "$cfg_dir_rootfs/boot" &&
 
-    for f in bootcode.bin fixup.dat start.elf; do
-        $cmd_cp -r "../$pkg-$pkg-$version/$f" "$cfg_dir_rootfs/boot"
+    for f in bootcode.bin fixup.dat start.elf start_cd.elf start_db.elf start_x.elf; do
+        $cmd_cp -r "../$pkg-$version/$f" "$cfg_dir_rootfs/boot"
     done &&
 
     tar -C "$pkg_dir/fs" -c -f - . | tar -C "$cfg_dir_rootfs" -x -v -f -
