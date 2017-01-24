a7df46d61a7c97a2e725bce9e0579354dd2b20df
Pedro Gonçalves <pedro@lsts.pt>
2017-01-24 19:59:42 +0000

    linux: update kernel to 4.4.10.

--- rules/linux/default.bash
+++ rules/linux/default.bash
@@ -1,11 +1,11 @@
 version=\
 (
-    '3.14.63'
+    '4.4.10'
 )
 
 url=\
 (
-    "https://www.kernel.org/pub/linux/kernel/v3.x/linux-$version.tar.xz"
+    "https://www.kernel.org/pub/linux/kernel/v4.x/linux-$version.tar.xz"
 )
 
 md5=\
