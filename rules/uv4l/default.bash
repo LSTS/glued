db04cf06cb6590c7231fcec7ea86fbb38f34dacb
Tiago Marques <samarques.tiago@gmail.com>
2017-06-02 10:27:27 +0100

    rules/uv4l: Don't hard code raspicam script.

--- rules/uv4l/default.bash
+++ rules/uv4l/default.bash
@@ -22,11 +22,6 @@ post_unpack()
 {
     mv "../etc" "../uv4l-$version/"
     mv "../usr" "../uv4l-$version/"
-    touch startStreamRaspiCam.sh
-    echo "#!/bin/sh" >> startStreamRaspiCam.sh
-    echo "killall uv4l" >> startStreamRaspiCam.sh
-    echo "modprobe cuse" >> startStreamRaspiCam.sh
-    echo "uv4l -nopreview --auto-video_nr --driver raspicam --encoding h264 --width 1080 --height 720 --framerate 12 --quality 8 --server-option '--port=9090' --server-option '--max-queued-connections=30' --server-option '--max-streams=25' --server-option '--max-threads=29'" >> startStreamRaspiCam.sh
 }
 
 host_install()
@@ -39,7 +34,8 @@ target_install()
 {
     $cmd_cp -r "usr/lib/"* "$cfg_dir_rootfs/usr/lib/"
     $cmd_cp -r "usr/bin/"* "$cfg_dir_rootfs/usr/bin/"
-    $cmd_cp -r "startStreamRaspiCam.sh" "$cfg_dir_rootfs/usr/bin/"
+
+    tar -C "$pkg_dir/fs" -c -f - . | tar -C "$cfg_dir_rootfs" -x -v -f -
 }
 
 
