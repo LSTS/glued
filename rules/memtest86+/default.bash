5a8237118f3753685be2eee282dda9ac8e9a5ff4
Tiago Marques <samarques.tiago@gmail.com>
2017-08-01 20:39:26 +0100

    rules: Added rule for memtext86+

---
+++ rules/memtest86+/default.bash
@@ -0,0 +1,31 @@
+version=\
+(
+    "5.01"
+)
+
+url=\
+(
+    "http://www.lsts.pt/glued/memtest86+-$version.tar.gz"
+)
+
+md5=\
+(
+    "035b85f2edabc9b4a6b0cf6c9c3acf50"
+)
+
+maintainer=\
+(
+    'Tiago Sá Marques <tsmarques@fe.up.pt>'
+)
+
+build()
+{
+    $cmd_make \
+        CC=$cmd_target_cc \
+        CXX=$cmd_target_cxx
+}
+
+target_install()
+{
+    $cmd_cp ./memtest.bin $cfg_dir_rootfs/boot/
+}
