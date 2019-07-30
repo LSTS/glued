3267ac919c6a2e6c3967f22d87356c52eac3bd7d
Pedro Gonçalves <pedro@lsts.pt>
2019-07-30 18:16:24 +0100

    rules:rpi-boot: update to version 4.19.60.

--- rules/rpi-boot-firmware/default.bash
+++ rules/rpi-boot-firmware/default.bash
@@ -1,6 +1,6 @@
 version=\
 (
-    '2019-02-25'
+    '2019-07-29'
 )
 
 url=\
@@ -10,20 +10,17 @@ url=\
 
 maintainer=\
 (
-    'Ricardo Martins <rasm@fe.up.pt>'
     'Pedro Gonçalves <pedro@lsts.pt>'
 )
 
 md5=\
 (
-    'da98308e12fa1eec23a1213d66cde30d'
+    '5b35887b1026fafe8174f473679d175a'
 )
 
 target_install()
 {
     $cmd_mkdir "$cfg_dir_rootfs/boot"
-
     $cmd_cp "../$pkg-$version/"* "$cfg_dir_rootfs/boot/"
-
     tar -C "$pkg_dir/fs" -c -f - . | tar -C "$cfg_dir_rootfs" -x -v -f -
 }
