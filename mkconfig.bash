09b319490dbd9e12be1dbc522a7ae680c6333765
Pedro Gonçalves <pedro@lsts.pt>
2019-08-14 18:00:39 +0100

    mkconfig: update version of id of glued.

--- mkconfig.bash
+++ mkconfig.bash
@@ -23,7 +23,7 @@
 ###########################################################################
 
 # Config: GLUED version.
-cfg_glued_version='2017.11'
+cfg_glued_version='2019.08'
 # Config: GLUED vendor.
 cfg_glued_vendor='lsts'
 
