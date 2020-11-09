e8b25683e6b21ae5111697fa1f549986ec23268e
Pedro Gonçalves <pedro@lsts.pt>
2020-11-09 19:46:55 +0000

    Rules/v4l-utils: update rule (lctr-rpi compatibility).

--- rules/v4l-utils/default.bash
+++ rules/v4l-utils/default.bash
@@ -1,6 +1,6 @@
 version=\
 (
-    0.8.8
+    1.20.0
 )
 
 url=\
@@ -10,8 +10,18 @@ url=\
 
 md5=\
 (
-    40fdda3f4055ed818012d7a7b5ef5be5
+    46f9e2c0b2fdccd009da2f7e1aa87894
 )
+
+configure()
+{
+    export PATH=$cfg_dir_toolchain/bin:$PATH
+    export INSTALL_PREFIX=$cfg_dir_toolchain_sysroot
+    ./configure \
+        --prefix="$cfg_dir_rootfs/usr" \
+        --host="$cfg_target_canonical" \
+        --without-jpeg
+}
 
 build()
 {
@@ -23,7 +33,7 @@ build()
 
 host_install()
 {
-    $cmd_make \
+    $cmd_make_single \
         CC=$cmd_target_cc \
         CXX=$cmd_target_cxx \
         PREFIX="$cfg_dir_toolchain_sysroot/usr" \
@@ -33,12 +43,12 @@ host_install()
 
 target_install()
 {
-    for f in "$cfg_dir_toolchain_sysroot/usr/lib/"libv4l*so*; do
-        $cmd_target_strip "$f" -o "$cfg_dir_rootfs/lib/$(basename "$f")"
+    for f in "$cfg_dir_rootfs/usr/lib/"libv4l*so*; do
+        $cmd_target_strip "$f" -o "$cfg_dir_toolchain_sysroot/usr/lib/$(basename "$f")"
     done
 
-    $cmd_mkdir "$cfg_dir_rootfs/lib/libv4l"
-    for f in "$cfg_dir_toolchain_sysroot/usr/lib/libv4l/"*; do
-        $cmd_target_strip "$f" -o "$cfg_dir_rootfs/lib/libv4l/$(basename "$f")"
+    $cmd_mkdir "$cfg_dir_toolchain_sysroot/usr/lib/libv4l"
+    for f in "$cfg_dir_rootfs/usr/lib/libv4l/"*; do
+        $cmd_target_strip "$f" -o "$cfg_dir_toolchain_sysroot/usr/lib/libv4l/$(basename "$f")"
     done
 }
