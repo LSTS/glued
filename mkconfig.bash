f5337f17fb81706e93937f671161fc82db362d6b
Pedro Gonçalves <pedro@lsts.pt>
2020-11-16 18:14:43 +0000

    mkconfig: update version info.

--- mkconfig.bash
+++ mkconfig.bash
@@ -23,7 +23,7 @@
 ###########################################################################
 
 # Config: GLUED version.
-cfg_glued_version='2017.11'
+cfg_glued_version='2020.12'
 # Config: GLUED vendor.
 cfg_glued_vendor='lsts'
 
