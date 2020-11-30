1cece77030b9b169a4e7640601afe401a3f17970
Pedro Gonçalves <pedro@lsts.pt>
2020-11-30 16:33:59 +0000

    Rules/rpi-boot-firmware: add rule for rpi4 boot.

---
+++ rules/rpi-boot-firmware/rpi4.bash
@@ -0,0 +1,28 @@
+version=\
+(
+    '2019-07-29'
+)
+
+url=\
+(
+    "http://lsts.pt/glued/rpi-boot-firmware-$version.zip"
+)
+
+maintainer=\
+(
+    'Ricardo Martins <rasm@fe.up.pt>'
+    'Pedro Gonçalves <pedro@lsts.pt>'
+)
+
+md5=\
+(
+    '5b35887b1026fafe8174f473679d175a'
+)
+
+target_install()
+{
+    eval "apt-get install -y libssl-dev bison kmod bc"
+    $cmd_mkdir "$cfg_dir_rootfs/boot"
+    $cmd_cp "../$pkg-$version/"* "$cfg_dir_rootfs/boot/"
+    tar -C "$pkg_dir/fs" -c -f - . | tar -C "$cfg_dir_rootfs" -x -v -f -
+}
