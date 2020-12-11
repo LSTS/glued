2f4372e26b46351a71d3b0357aa5238b6e517fb8
Pedro Gonçalves <pedro@lsts.pt>
2020-12-11 20:59:28 +0000

    Rules/rpi-boot-firmware/rpi4: add flex.

--- rules/rpi-boot-firmware/rpi4.bash
+++ rules/rpi-boot-firmware/rpi4.bash
@@ -21,7 +21,7 @@ md5=\
 
 target_install()
 {
-    eval "apt-get install -y libssl-dev bison kmod bc"
+    eval "apt-get install -y libssl-dev bison kmod bc flex"
     $cmd_mkdir "$cfg_dir_rootfs/boot"
     $cmd_cp "../$pkg-$version/"* "$cfg_dir_rootfs/boot/"
     tar -C "$pkg_dir/fs" -c -f - . | tar -C "$cfg_dir_rootfs" -x -v -f -
