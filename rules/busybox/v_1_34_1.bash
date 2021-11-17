487d2dfe0b6b33f94e67d4ef43897256c067d6b1
Paulo Dias <paulo.sousa.dias@gmail.com>
2021-11-17 12:21:13 +0000

    rules/busybox: Give error if no env var is set (cfg_lsts_ftp_logs_path).

--- rules/busybox/v_1_34_1.bash
+++ rules/busybox/v_1_34_1.bash
@@ -35,9 +35,8 @@ build()
 
 target_install()
 {
-    echo $(pwd)
     rm -Rf .tmpftp && mkdir -p .tmpftp/etc && [[ ! -z "$cfg_lsts_ftp_logs_path" ]] \
-        && echo "30021 stream  tcp     nowait  root    /usr/sbin/ftpd /usr/sbin/ftpd -a root -w $cfg_lsts_ftp_logs_path" > .tmpftp/etc/lsts-ftp-logs.conf
+        && echo "30021 stream  tcp     nowait  root    /usr/sbin/ftpd /usr/sbin/ftpd -a root -w $cfg_lsts_ftp_logs_path" > .tmpftp/etc/lsts-ftp-logs.conf || echo 'Missing "$cfg_lsts_ftp_logs_path" var set!' && exit 1
 
     $cmd_make CROSS_COMPILE="$cfg_target_canonical"- CONFIG_PREFIX=$cfg_dir_rootfs install &&
     tar -C "$pkg_dir/fs" --exclude .svn -c -f - . | tar -C "$cfg_dir_rootfs" -x -v -f - &&
