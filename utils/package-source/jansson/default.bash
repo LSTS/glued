5f27b0b2f20fa1197659ecfa780c36c4459990a3
Pedro Gonçalves <pedro@lsts.pt>
2022-03-08 19:09:50 +0000

    utils/package-source/jansson: update rule to create repository package.

--- utils/package-source/jansson/default.bash
+++ utils/package-source/jansson/default.bash
@@ -5,7 +5,7 @@ version=\
 
 url=\
 (
-    "http://www.digip.org/jansson/releases/jansson-2.7.tar.gz"
+    "http://www.digip.org/jansson/releases/jansson-$version.tar.gz"
 )
 
 md5=\
@@ -15,8 +15,15 @@ md5=\
 
 configure()
 {
+    mkdir $cfg_dir_builds/$pkg/toolchain
+    mkdir $cfg_dir_builds/$pkg/toolchain/$cfg_target_canonical
+    mkdir $cfg_dir_builds/$pkg/toolchain/$cfg_target_canonical/sysroot
+    mkdir $cfg_dir_builds/$pkg/toolchain/$cfg_target_canonical/sysroot/usr
+    mkdir $cfg_dir_builds/$pkg/toolchain/$cfg_target_canonical/sysroot/usr/lib
+    export cfg_dir_output_toolchain_sysroot=$cfg_dir_builds/$pkg/toolchain/$cfg_target_canonical/sysroot
+
     ./configure \
-        --prefix="${cfg_dir_toolchain_sysroot}/usr" \
+        --prefix="${cfg_dir_output_toolchain_sysroot}/usr" \
         --target="$cfg_target_canonical" \
         --host="$cfg_target_canonical" \
         --build="$cfg_host_canonical"
@@ -34,12 +41,19 @@ host_install()
 
 target_install()
 {
+  mkdir $cfg_dir_builds/$pkg/rootfs
+  mkdir $cfg_dir_builds/$pkg/rootfs/usr
+  mkdir $cfg_dir_builds/$pkg/rootfs/usr/lib
+  export cfg_dir_output_rootfs=$cfg_dir_builds/$pkg/rootfs
+
   # Libs:
-  for f in "$cfg_dir_toolchain_sysroot/usr/lib/"libjansson.*; do
+  for f in "$cfg_dir_output_toolchain_sysroot/usr/lib/"libjansson.*; do
     if [ -L "$f" ]; then
-      $cmd_cp "$f" "$cfg_dir_rootfs/usr/lib"
+      $cmd_cp "$f" "$cfg_dir_output_rootfs/usr/lib"
     else
-      $cmd_target_strip -v "$f" -o "$cfg_dir_rootfs/usr/lib/$(basename "$f")"
+      $cmd_target_strip -v "$f" -o "$cfg_dir_output_rootfs/usr/lib/$(basename "$f")"
     fi
   done
+  
+  tar -czf ../jansson-v$version.tar.gz ../rootfs ../toolchain
 }
