26a25e1ec8a3b3275ca3389a0d9aaeb4acf3a63e
Pedro Gonçalves <pedro@lsts.pt>
2022-03-08 19:10:13 +0000

    rules: add rule jansson.

---
+++ rules/jansson/default.bash
@@ -0,0 +1,25 @@
+version=\
+(
+    "v2.7"
+)
+
+maintainer=\
+(
+    'Pedro Gonçalves <pedro@lsts.pt>'
+)
+
+url=\
+(
+    "https://www.lsts.pt/glued/cloud/package/lctr-rpi4/jansson-${version}.tar.gz"
+)
+
+md5=\
+(
+    "57b13be9f1b4f54149c7f8be173b7613"
+)
+
+post_unpack()
+{
+    $cmd_cp ../rootfs/* $cfg_dir_rootfs
+    $cmd_cp ../toolchain/* $cfg_dir_toolchain
+}
