0cc4638f4fd4e3158f4b53a4f6123a3ce5936670
Pedro Gonçalves <pedro@lsts.pt>
2022-03-08 19:55:31 +0000

    rules: add libtool rpi4-host.

---
+++ rules/libtool/rpi4-host.bash
@@ -0,0 +1,24 @@
+version=\
+(
+    'v2.4.6'
+)
+
+maintainer=\
+(
+    'Pedro Gonçalves <pedro@lsts.pt>'
+)
+
+url=\
+(
+    "https://www.lsts.pt/glued/cloud/package/lctr-rpi4/libtool-$version.tar.gz"
+)
+
+md5=\
+(
+    '922293162e06b392472d2aff06502467'
+)
+
+post_unpack()
+{
+    $cmd_cp ../toolchain/* $cfg_dir_toolchain
+}
