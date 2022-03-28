1dabc954d44fd2ade59533c47d49f0646aae1f2a
Pedro Gonçalves <pedro@lsts.pt>
2022-03-28 17:19:45 +0100

    rules/nfs-utils: update rule for cloud build.

---
+++ rules/nfs-utils/rpi4.bash
@@ -0,0 +1,29 @@
+version=\
+(
+    'v1.3.2'
+)
+
+url=\
+(
+    "http://downloads.sourceforge.net/nfs/$version/nfs-utils-$version.tar.bz2"
+)
+
+url=\
+(
+    "https://www.lsts.pt/glued/cloud/package/lctr-rpi4/nfs-utils-$version.tar.gz"
+)
+
+requires=\
+(
+    'libtirpc/rpi4'
+)
+
+md5=\
+(
+    'e38ecfa598d2f619027b6b329f72708c'
+)
+
+post_unpack()
+{
+    $cmd_cp ../rootfs/* $cfg_dir_rootfs
+}
--- utils/package-source/nfs-utils/default.bash
+++ utils/package-source/nfs-utils/default.bash
@@ -20,7 +20,7 @@ maintainer=\
 
 requires=\
 (
-    'libtirpc/default'
+    'libtirpc/rpi4'
 )
 
 post_unpack()
@@ -35,11 +35,18 @@ post_unpack()
 
 configure()
 {
+    mkdir ../rootfs
+    mkdir ../rootfs/etc
+    mkdir ../rootfs/usr
+    mkdir ../rootfs/usr/bin
+    mkdir ../rootfs/usr/sbin
+    export cfg_dir_output_rootfs=$cfg_dir_builds/$pkg/rootfs
+
     "../nfs-utils-$version/configure" \
         --target="$cfg_target_canonical" \
         --host="$cfg_target_canonical" \
         --build="$cfg_host_canonical" \
-        --prefix="$cfg_dir_rootfs/usr" \
+        --prefix="$cfg_dir_output_rootfs/usr" \
         --disable-gss \
         --disable-svcgss \
         --disable-ipv6 \
@@ -56,12 +63,14 @@ build()
 
 target_install()
 {
-    $cmd_target_strip -v "utils/mount/mount.nfs" -o "$cfg_dir_rootfs/usr/bin/mount.nfs" &&
-    ln -fs mount.nfs "$cfg_dir_rootfs/usr/bin/unmount.nfs" &&
-    $cmd_target_strip -v "utils/exportfs/exportfs" -o "$cfg_dir_rootfs/usr/bin/exportfs" &&
-    $cmd_target_strip -v "utils/mountd/mountd" -o "$cfg_dir_rootfs/usr/bin/rpc.mountd" &&
-    $cmd_target_strip -v "utils/nfsd/nfsd" -o "$cfg_dir_rootfs/usr/bin/rpc.nfsd" &&
-    $cmd_target_strip -v "utils/statd/statd" -o "$cfg_dir_rootfs/usr/bin/rpc.statd" &&
-    $cmd_target_strip -v "utils/statd/sm-notify" -o "$cfg_dir_rootfs/usr/sbin/sm-notify" &&
-    touch "$cfg_dir_rootfs/etc/exports"
+    $cmd_target_strip -v "utils/mount/mount.nfs" -o "$cfg_dir_output_rootfs/usr/bin/mount.nfs" &&
+    ln -fs mount.nfs "$cfg_dir_output_rootfs/usr/bin/unmount.nfs" &&
+    $cmd_target_strip -v "utils/exportfs/exportfs" -o "$cfg_dir_output_rootfs/usr/bin/exportfs" &&
+    $cmd_target_strip -v "utils/mountd/mountd" -o "$cfg_dir_output_rootfs/usr/bin/rpc.mountd" &&
+    $cmd_target_strip -v "utils/nfsd/nfsd" -o "$cfg_dir_output_rootfs/usr/bin/rpc.nfsd" &&
+    $cmd_target_strip -v "utils/statd/statd" -o "$cfg_dir_output_rootfs/usr/bin/rpc.statd" &&
+    $cmd_target_strip -v "utils/statd/sm-notify" -o "$cfg_dir_output_rootfs/usr/sbin/sm-notify" &&
+    touch "$cfg_dir_output_rootfs/etc/exports"
+
+    tar -czf ../nfs-utils-v$version.tar.gz ../rootfs
 }
