b43fd9ffb11cbfceebc8247414d741da648fc86b
Pedro Gonçalves <pedro@lsts.pt>
2022-04-21 15:06:39 +0100

    rules/syslinux: update rule for a6xx cloud build.

--- rules/syslinux/a6xx.bash
+++ rules/syslinux/a6xx.bash
@@ -29,3 +29,13 @@ post_unpack()
     $cmd_cp ../rootfs/* $cfg_dir_rootfs
     $cmd_cp ../toolchain/* $cfg_dir_toolchain
 }
+
+postconfigure()
+{
+    rm -rf $cfg_dir_builds/$pkg/toolchain
+    rm -rf $cfg_dir_builds/$pkg/rootfs
+    rm -rf $cfg_dir_builds/$pkg/syslinux-$version
+    rm -rf $cfg_dir_builds/$pkg/.download
+    rm -rf $cfg_dir_builds/$pkg/.post_unpack
+    rm -rf $cfg_dir_builds/$pkg/.unpack
+}
