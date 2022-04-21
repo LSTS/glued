4b869f4e9a8bc609d2244fc6f99a26d2925517b1
Pedro Gonçalves <pedro@lsts.pt>
2022-04-21 15:09:18 +0100

    utils/package/syslinux: update default builds to create package for cloud build.

--- utils/package/syslinux/default.bash
+++ utils/package/syslinux/default.bash
@@ -2,13 +2,20 @@ source $pkg_common
 
 host_install()
 {
+    mkdir -p $cfg_dir_builds/$pkg/toolchain/bin/
+    export cfg_dir_output_toolchain=$cfg_dir_builds/$pkg/toolchain
+
     $cmd_make clean &&
     $cmd_make &&
-    cp -d extlinux/extlinux ${cfg_dir_toolchain}/bin/extlinux
+    cp -d extlinux/extlinux ${cfg_dir_output_toolchain}/bin/extlinux
 }
 
 target_install()
 {
+    mkdir -p $cfg_dir_builds/$pkg/rootfs/boot
+    mkdir -p $cfg_dir_builds/$pkg/rootfs/usr/sbin
+    export cfg_dir_output_rootfs=$cfg_dir_builds/$pkg/rootfs
+
     if [ -n "$cfg_terminal" ]; then
         cfg_kernel_extra_args="$cfg_kernel_extra_args console=$cfg_terminal,115200"
         port="$(echo $cfg_terminal | sed 's/ttyS//g')"
@@ -21,11 +28,13 @@ target_install()
 
     $cmd_make clean &&
     $cmd_make CC="$cmd_target_cc" &&
-    $cmd_target_strip extlinux/extlinux -o $cfg_dir_rootfs/usr/sbin/extlinux &&
-    $cmd_mkdir $cfg_dir_rootfs/boot/extlinux &&
-    cp -d mbr/mbr.bin $cfg_dir_rootfs/boot/extlinux &&
+    $cmd_target_strip extlinux/extlinux -o $cfg_dir_output_rootfs/usr/sbin/extlinux &&
+    $cmd_mkdir $cfg_dir_output_rootfs/boot/extlinux &&
+    cp -d mbr/mbr.bin $cfg_dir_output_rootfs/boot/extlinux &&
     (echo $console ; cat "$pkg_dir"/extlinux.conf) \
          | sed "s%\$cfg_kernel_extra_args%$cfg_kernel_extra_args%g" \
          | sed "s%\$cfg_kernel_boot_dev%$cfg_kernel_boot_dev%g" \
-         >  "$cfg_dir_rootfs/boot/extlinux/extlinux.conf"
+         >  "$cfg_dir_output_rootfs/boot/extlinux/extlinux.conf"
+
+    tar -czf ../syslinux-v$version.tar.gz ../rootfs ../toolchain
 }
--- utils/package/syslinux/host.bash
+++ utils/package/syslinux/host.bash
@@ -2,9 +2,14 @@ source $pkg_common
 
 host_install()
 {
+    mkdir -p $cfg_dir_builds/$pkg/toolchain/bin
+    export cfg_dir_output_toolchain=$cfg_dir_builds/$pkg/toolchain
+
     $cmd_make clean &&
     $cmd_make &&
-    cp -d extlinux/extlinux ${cfg_dir_toolchain}/bin &&
-    mkdir -p ${cfg_dir_toolchain}/boot &&
-    cp -d mbr/mbr.bin ${cfg_dir_toolchain}/boot/mbr.bin
+    cp -d extlinux/extlinux ${cfg_dir_output_toolchain}/bin &&
+    mkdir -p ${cfg_dir_output_toolchain}/boot &&
+    cp -d mbr/mbr.bin ${cfg_dir_output_toolchain}/boot/mbr.bin
+
+    tar -czf ../syslinux-v$version-host.tar.gz ../toolchain
 }
