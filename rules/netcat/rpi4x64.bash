b306c8b5680834326f6636c5e1e053be6d27fe13
Pedro Gonçalves <pedro@lsts.pt>
2022-05-31 15:38:51 +0100

    rules: add rule netcat and readline for rpi4x64.

---
+++ rules/netcat/rpi4x64.bash
@@ -0,0 +1,25 @@
+version=\
+(
+    "v110"
+)
+
+maintainer=\
+(
+    'Pedro Gonçalves <pedro@lsts.pt>'
+)
+
+url=\
+(
+    "https://www.lsts.pt/glued/cloud/package/lctr-rpi4x64/gcc-5.4/netcat-${version}.tar.gz"
+)
+
+md5=\
+(
+    "55f08a99edb832c85c6c8c5c6dc37dd3"
+)
+
+post_unpack()
+{
+    $cmd_cp ../rootfs/* $cfg_dir_rootfs
+    $cmd_cp ../toolchain/* $cfg_dir_toolchain
+}
---
+++ rules/readline/rpi4x64.bash
@@ -0,0 +1,25 @@
+version=\
+(
+    'v8.0'
+)
+
+maintainer=\
+(
+    'Pedro Gonçalves <pedro@lsts.pt>'
+)
+
+url=\
+(
+    "https://www.lsts.pt/glued/cloud/package/lctr-rpi4x64/gcc-5.4/readline-${version}.tar.gz"
+)
+
+md5=\
+(
+    "bb4e8ea6bd6f6bbcb72faa5ef029f485"
+)
+
+post_unpack()
+{
+    $cmd_cp ../rootfs/* $cfg_dir_rootfs
+    $cmd_cp ../toolchain/* $cfg_dir_toolchain
+}
